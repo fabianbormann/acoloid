271c2fadaad5fc21bbb4494b0b032740df554ce3
fabian <fabian_mb@gmx.de>
2013-04-21 18:48:29 +0200

    change the gui and the main class
    tested on my device (SGS2 - 4.2.2 Jelly Bean)

--- gen/com/example/acoloid/R.java
+++ gen/com/example/acoloid/R.java
@@ -24,12 +24,11 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080005;
+        public static final int action_settings=0x7f080004;
         public static final int seekBar1=0x7f080001;
         public static final int seekBar2=0x7f080002;
         public static final int seekBar3=0x7f080003;
         public static final int textView1=0x7f080000;
-        public static final int toggleButton1=0x7f080004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -40,7 +39,7 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int info_text=0x7f050002;
     }
     public static final class style {
         /** 
--- src/com/example/acoloid/MainActivity.java
+++ src/com/example/acoloid/MainActivity.java
@@ -1,11 +1,21 @@
 package com.example.acoloid;
 
+import java.io.OutputStream;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothSocket;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
-
+	
+	private BluetoothAdapter bAdapter = null;
+	private BluetoothSocket bSocket = null;
+	private OutputStream outColor = null;
+	
+	private static String bMAC = "00:00:00:00:00";
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
